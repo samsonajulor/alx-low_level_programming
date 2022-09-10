67299d411ff268981b3de62393416268e0543f13
samsonajulor <s.ajulor@abbeymortgagebank.com>
2022-09-10 10:14:43 +0100

    completed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -26,8 +26,7 @@ int main(void)
 				putchar((digit3 % 10) + '0');
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
+				continue;
 				putchar(',');
 				putchar(' ');
 			}
