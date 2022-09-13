139760df21bf6046b7fb1d082f99ab1f2e75e4c3
samsonajulor <s.ajulor@abbeymortgagebank.com>
2022-09-13 12:21:13 +0100

    fixed syntax

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,15 +15,15 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
+			_putchar(n + '0');
 			n--;
-		_putchar(n + '0');
 	}
 
 	else
 	{
 		while (n < 98)
+			_putchar(n + '0');
 			n++;
-		_putchar(n + '0');
 	}
 }
 
